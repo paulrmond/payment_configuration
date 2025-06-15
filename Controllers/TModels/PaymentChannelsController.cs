e257ebdcc8975d3887eabd3fec77b7205e38e628
Paul Balena <paulrmond13@gmail.com>
2025-06-15 18:45:31 +0800

    add transaction controllers

---
+++ Controllers/TModels/PaymentChannelsController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentOptions.Data;
+using PaymentOptions.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaymentOptions.Controllers.TModels
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentChannelsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentChannelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<PaymentChannelController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentChannel>>> GetPaymentChannel()
+        {
+            return await _context.PaymentChannels.ToListAsync();
+        }
+
+        // GET api/<PaymentChannelController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentChannel>> GetPaymentChannel(int id)
+        {
+            var paymentChannel = await _context.PaymentChannels.FindAsync(id);
+            if (paymentChannel == null)
+            {
+                return NotFound();
+            }
+            return paymentChannel;
+        }
+
+        // POST api/<PaymentChannelController>
+        [HttpPost]
+        public async Task<ActionResult<PaymentChannel>> PostPaymentChannel(PaymentChannel paymentChannel)
+        {
+            _context.PaymentChannels.Add(paymentChannel);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPaymentChannel", new { id = paymentChannel.PaymentChannelId }, paymentChannel);
+        }
+
+        // PUT api/<PaymentChannelController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentChannel(int id, PaymentChannel paymentChannel)
+        {
+            if (id != paymentChannel.PaymentChannelId) { 
+                return BadRequest();
+            }
+
+            PaymentChannel _paymentChannel = new PaymentChannel { 
+                PaymentChannelId = paymentChannel.PaymentChannelId,
+                ChannelCode = paymentChannel.ChannelCode,
+                ChannelName = paymentChannel.ChannelName,
+                CreatedBy = paymentChannel.CreatedBy,
+                DateCreated = paymentChannel.DateCreated,
+                DateModified = paymentChannel.DateModified,
+                IsActive = paymentChannel.IsActive,
+                IsPartialPayment = paymentChannel.IsPartialPayment,
+                ModifiedBy = paymentChannel.ModifiedBy
+            };
+
+            _context.Entry(_paymentChannel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex) {
+                throw;
+            }
+            return NoContent();
+        }
+
+        // DELETE api/<PaymentChannelController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentChannel(int id)
+        {
+            var _paymentChannel = await _context.FindAsync<PaymentChannel>(id);
+            if (_paymentChannel != null)
+            {
+                _paymentChannel.IsActive = false;
+                _context.Entry(_paymentChannel).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Controllers/TModels/PaymentTabsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentOptions.Data;
+using PaymentOptions.Model;
+
+namespace PaymentOptions.Controllers.TModels
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTabsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentTabsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentTabs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentTab>>> GetPaymentTabs()
+        {
+            return await _context.PaymentTabs.ToListAsync();
+        }
+
+        // GET: api/PaymentTabs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentTab>> GetPaymentTab(int id)
+        {
+            var paymentTab = await _context.PaymentTabs.FindAsync(id);
+
+            if (paymentTab == null)
+            {
+                return NotFound();
+            }
+
+            return paymentTab;
+        }
+
+        // PUT: api/PaymentTabs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentTab(int id, PaymentTab paymentTab)
+        {
+            if (id != paymentTab.PaymentTabId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentTab).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentTabExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentTabs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentTab>> PostPaymentTab(PaymentTab paymentTab)
+        {
+            _context.PaymentTabs.Add(paymentTab);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentTab", new { id = paymentTab.PaymentTabId }, paymentTab);
+        }
+
+        // DELETE: api/PaymentTabs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentTab(int id)
+        {
+            var paymentTab = await _context.PaymentTabs.FindAsync(id);
+            if (paymentTab == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentTabs.Remove(paymentTab);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentTabExists(int id)
+        {
+            return _context.PaymentTabs.Any(e => e.PaymentTabId == id);
+        }
+    }
+}
